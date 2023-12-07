83747ba4796355801f260c13f92dc0219966566f
bahaehmimdi <48775632+bahaehmimdi@users.noreply.github.com>
2023-12-07 12:16:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,22 @@
+# app.py
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*", allowEIO3=True)
+socketio = SocketIO(app)
 
-@app.route('/start_call')
-def start_call():
-    return render_template('start_call.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-@app.route('/receive_call')
-def receive_call():
-    return render_template('receive_call.html')
+@app.route('/page1')
+def page1():
+    return render_template('page1.html')
 
-@socketio.on('start_call')
-def handle_start_call(message):
-    socketio.emit('start_call', message)
-
-@socketio.on('receive_call')
-def handle_receive_call(message):
-    socketio.emit('receive_call', message)
+@socketio.on('audio_data')
+def handle_audio(audio_data):
+    # Broadcast the received audio data to all connected clients
+    socketio.emit('audio_data', audio_data, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
-
