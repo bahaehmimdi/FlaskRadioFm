1d3b7ffe44f5c4a0e6a1c371372765ac82cf55f1
bahaehmimdi <48775632+bahaehmimdi@users.noreply.github.com>
2023-12-07 02:37:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app, cors_allowed_origins="*", allowEIO3=True)
 
 @app.route('/start_call')
 def start_call():
