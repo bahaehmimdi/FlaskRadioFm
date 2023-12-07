95bd0deb7ac5273bedbcecd6c7b847b197652595
bahaehmimdi <48775632+bahaehmimdi@users.noreply.github.com>
2023-12-07 13:21:12 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # app.py
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask, render_template, Response, request
+from flask_socketio import SocketIO, emit
+import base64
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -15,7 +16,14 @@ def receiver():
 
 @socketio.on('audio_data')
 def handle_audio(audio_data):
-    socketio.emit('audio_data', audio_data, broadcast=True)
+    emit('audio_data', audio_data, broadcast=True)
+
+@app.route('/record_audio', methods=['POST'])
+def record_audio():
+    audio_data = request.data
+    encoded_audio = base64.b64encode(audio_data).decode('utf-8')
+    socketio.emit('audio_data', encoded_audio, broadcast=True)
+    return "OK"
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
